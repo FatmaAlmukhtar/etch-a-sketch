9d269a29ca6868858a07c3bd84d663fb21465539
Fatma Almukhtar <fatma.almukhtar18@gmail.com>
2022-07-03 16:58:52 -0600

    change grid-cell color when mouseover the cell

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 let container = document.getElementById('container');
+let grid = document.getElementsByClassName('grid');
 
 for(let i=1; i<=256; i++) {
     newDiv = document.createElement('div');
     newDiv.classList.add('grid', `${i}`);
     container.appendChild(newDiv);
+}
+
+for(let i=0; i<grid.length; i++) {
+    grid.item(i).addEventListener('mouseover', () => {
+        grid.item(i).style.backgroundColor = 'red';
+    })
 }
