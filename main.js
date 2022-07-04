d276471df11f7692e1ba045a8c33d92c8cdfc6ef
Fatma Almukhtar <fatma.almukhtar18@gmail.com>
2022-07-03 19:38:48 -0600

    add button to choose grid size, remove grid and generate new one according to the user size choice

--- main.js
+++ main.js
@@ -1,14 +1,41 @@
 let container = document.getElementById('container');
-let grid = document.getElementsByClassName('grid');
+let grid = document.querySelectorAll('grid');
+let newGrid = document.querySelector('button');
 
-for(let i=1; i<=256; i++) {
-    newDiv = document.createElement('div');
-    newDiv.classList.add('grid', `${i}`);
-    container.appendChild(newDiv);
+window.addEventListener('load', () => {
+    generateGrid(16);
+    activatePen();
+})
+
+function generateGrid(size) {
+    
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    container.style.gridTemplateColumns = `repeat(${size}, 10px)`;
+    container.style.gridTemplateRows = `repeat(${size}, 10px)`;
+
+    for(let i=1; i<=(size*size); i++) {
+        newDiv = document.createElement('div');
+        newDiv.classList.add('grid', `${i}`);
+        container.appendChild(newDiv);
+    }
+    grid = document.getElementsByClassName('grid');
 }
 
-for(let i=0; i<grid.length; i++) {
-    grid.item(i).addEventListener('mouseover', () => {
-        grid.item(i).style.backgroundColor = 'red';
-    })
+function activatePen() {
+    for(let i=0; i<grid.length; i++) {
+        grid.item(i).addEventListener('mouseover', () => {
+            grid.item(i).style.backgroundColor = 'red';
+        })
+    }
 }
+
+newGrid.addEventListener('click', () => {
+    size = prompt('Enter the grid size');
+    if(size<=100) {
+        generateGrid(size);
+        activatePen();
+    }
+    else alert('Please choose a grid size no larger than 100 pixels'); 
+})
